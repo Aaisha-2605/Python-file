53499c77680936df07baff4d32305d6053d2e084
Aaisha Anzar <aaishaanzar7@gmail.com>
2024-12-16 14:12:03 -0600

    Add files via upload

---
+++ python.py
@@ -0,0 +1,53 @@
+# Input Function question
+
+
+# a = input("Enter your name: ")
+# print(f"Welcome {a} to the world of Python")
+# age=input( "Enter your age: ")
+# print(f"oh, you are just {age} years old.")
+# print(f"It means you will be {int(age)+1} next year. You are too young.")
+# x= input ("Enter your first number: ")
+# y = input ("Enter your second number: ")
+# print(int(x)+(int(y)))
+# x= input ("Enter your first number: ")
+# y = input ("Enter your second number: ")
+# print(f"sum of {int(x)}+{int(y)} is {int(x) + int(y)} :")
+
+
+#Assignment 2 (practice session)
+
+# Q.1: write a python program to input student name and marks of 3 subjects. Print name & percentage in output
+
+# name = input("Enter your name: ")
+# maths= input("Enter your marks in maths: ")
+# hindi= input("Enter your marks in hindi: ")
+# english= input("Enter your marks in english: ")
+# totalpercentage=((int(maths) + int(hindi) + int(english))/300)*100
+# #calculating percentage
+# print(f"Wow your name is {name}. This is a beautiful name.")
+# print(f"So, the total percentage is {int(totalpercentage)} % is your percentage. Well done! ")
+
+# # Optimized solution
+# print("Percentage calculator")
+# name = input("Enter your beautiful name: ")
+# maths= int(input("Enter your maths marks: "))
+# hindi= int(input("Enter your hindi marks: "))
+# english= int(input("Enter your english marks: "))
+# #calculating percentage
+# percentage= (((maths + hindi + english)/ 300)*100)
+# print(f"The result of {name} is {percentage}% has shown that she is a bright student.")
+
+# Q.2. Write a python program that collects multiple types of data (e.g., name, age, height, and student status) from user input
+# and stores them in a dictionary, and then prints out the collected data.
+
+# print("Creating a dictionary")
+# name = dict(input("Enter your beautiful name: "))
+# age = dict(input("Enter your age: "))
+# height = dict(input("Enter your height: "))
+# status = dict(input("Enter your Student status: "))
+
+# dictionary= dict(name: , age:, height: , status:)
+
+
+
+
